c383f4afc214008c375a24db5db28f5e1ddcd6e3
dhilst <danielhilst@gmail.com>
2019-04-20 11:56:47 -0300

    Fix db name

--- db/config.js
+++ db/config.js
@@ -2,19 +2,20 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient 
 //const uri = "mongodb://localhost/batchfilter";
 const uri = process.env.MONGODB_URI;
+const dbname  = uri.split('/').reverse()[0];
 
 module.exports = {
 	connect(callback) {
     return MongoClient.connect(uri, (err, db) => {
       if (err) throw err;
-      return callback(db, db.db("batchfilter"));
+      return callback(db, db.db(dbname));
     });
   },
 
   savedfilters(callback) {
     return MongoClient.connect(uri, (err, db) => {
       if (err) throw err;
-      return callback(db, db.db("batchfilter").collection("savedfilters"));
+      return callback(db, db.db(dbname).collection("savedfilters"));
     });
   },
 
